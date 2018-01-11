83f63e9c36307e3c7a13a6b09f23f3d0e38a6f5b
Jamie Morgan-Ward <jamie@blacksheepdesign.co.nz>
2018-01-12 11:52:36 +1300

    Added variables into expect script

--- utils/expect-script.sh
+++ utils/expect-script.sh
@@ -1,7 +1,35 @@
 #! /usr/bin/expect -f
+
+# Get project details
+PACKAGE_NAME=$(cat package.json \
+  | grep name \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g')
+PACKAGE_DESCRIPTION=$(cat package.json \
+  | grep description \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g')
+PACKAGE_AUTHOR=$(cat package.json \
+  | grep author \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g')
+if grep -q lint package.json; then
+	PACKAGE_LINT=true
+else
+	PACKAGE_LINT=false
+fi
+
+echo $PACKAGE_NAME
+echo $PACKAGE_DESCRIPTION
+echo $PACKAGE_LINT
+echo $PACKAGE_AUTHOR
+
 set timeout 360
 
-spawn ./node_modules/.bin/vue init . $curdir
+spawn ./node_modules/.bin/vue init . $PACKAGE_NAME
 
 # This happens because of
 # https://github.com/vuejs/vue-cli/issues/291
